fc1746006739aaf526ebe1c5f658ed174dd7dfbf
A.H. Cooperstone <ahc2188@columbia.edu>
2025-06-29 22:49:11 -0400

    Add missing __init__.py for benchmarks module to fix imports

---
+++ src/quantum_nn/benchmarks/__init__.py
@@ -0,0 +1,7 @@
+"""Benchmarking utilities for quantum neural networks."""
+
+from .benchmark_runner import BenchmarkRunner
+from .classical_comparison import run_classical_comparison
+from .tasks import get_benchmark_task
+
+__all__ = ["BenchmarkRunner", "run_classical_comparison", "get_benchmark_task"]
