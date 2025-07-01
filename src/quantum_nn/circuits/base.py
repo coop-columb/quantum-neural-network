7783d12afcd6160ac178f0ffa911488747fc2053
A.H. Cooperstone <ahc2188@columbia.edu>
2025-06-30 23:41:55 -0400

    Create base.py
    feat: add base QuantumCircuitTemplate to decouple templates and circuits modules

---
+++ src/quantum_nn/circuits/base.py
@@ -0,0 +1,18 @@
+"""
+Base classes for quantum neural network circuits.
+"""
+
+from typing import List
+import numpy as np
+
+class QuantumCircuitTemplate:
+    """Base class for quantum circuit templates."""
+
+    def __init__(self, n_qubits: int):
+        self.n_qubits = n_qubits
+
+    def apply(self, params: np.ndarray, wires: List[int]):
+        raise NotImplementedError("Subclasses must implement apply method")
+
+    def parameter_count(self) -> int:
+        raise NotImplementedError("Subclasses must implement parameter_count method")
